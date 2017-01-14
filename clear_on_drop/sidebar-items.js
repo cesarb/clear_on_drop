0ad37701d28710f352fe24849516183d52e67f1d
Cesar Eduardo Barros <cesarb@cesarb.eti.br>
2017-01-14 19:31:51 -0200

    Initial commit

---
+++ clear_on_drop/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["clear_stack","Overwrites a few pages of stack."],["clear_stack_on_return","Calls a closure and overwrites its stack on return."],["clear_stack_on_return_fnonce","Calls a closure and overwrites its stack on return."]],"struct":[["ClearOnDrop","Zeroizes a storage location when dropped."]]});
---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["clear_on_drop"] = ["impl&lt;T:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a>,&nbsp;P:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a>&lt;Target=T&gt; + <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> + <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='clear_on_drop/struct.ClearOnDrop.html' title='clear_on_drop::ClearOnDrop'>ClearOnDrop</a>&lt;T,&nbsp;P&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
