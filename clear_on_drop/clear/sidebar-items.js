97ab161641c7ee4ce4a7825b4a106b107244c9e0
Cesar Eduardo Barros <cesarb@cesarb.eti.br>
2017-01-24 08:35:15 -0200

    Release 0.2

---
+++ clear_on_drop/clear/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"trait":[["Clear","An operation to completely overwrite a value, without leaking data."],["InitializableFromZeroed","A type that can be initialized to a valid value, after being set to all-bits-zero."],["ZeroSafe","Unsafe trait to indicate which types are safe to set to all-bits-zero."]]});
