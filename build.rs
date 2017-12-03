ddf89d590a16db66bcba80b850b2a11fc6a62294
Cesar Eduardo Barros <cesarb@cesarb.eti.br>
2017-12-03 21:00:31 -0200

    Update gcc dependency to 1.0

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
-extern crate gcc;
+extern crate cc;
 
 fn main() {
     if !cfg!(feature = "no_cc") {
-        gcc::compile_library("libclear_on_drop.a", &["src/hide.c"]);
+        cc::Build::new()
+            .file("src/hide.c")
+            .compile("clear_on_drop");
     }
 }
